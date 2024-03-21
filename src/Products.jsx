f3dd5ee430c337b0a7f406c899ddf5bcf28a5134
sarthakgupta15 <sarthakgupta.univ@gmail.com>
2024-03-21 10:36:40 +0530

    Added Products Component

---
+++ src/Products.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Products() {
+  return (
+    <div className="text-white">
+        <div className='w-full h-96 bg-zinc-800'></div>
+    </div>
+  )
+}
+
+export default Products
