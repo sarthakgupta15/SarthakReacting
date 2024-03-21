a714e24bfd6ac18f5609b6ff21b91fcbbc4653a7
sarthakgupta15 <sarthakgupta.univ@gmail.com>
2024-03-21 10:57:28 +0530

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-//
+import React, { useState } from 'react'
+
 function App() {
+  var [a,b] = useState(1);
   return (
-    <div> 
-      <div className = "main"></div> //html wali class not js wali
-      <h1>Hey</h1>
+    <div className='w-full h-screen bg-zinc-900 text-white p-5'>
+      <h1>{a}</h1>
+      <button onclick={()=>b(a+1)} className='px-3 py-1 bg-green-500 rounded-md text-xs'>Click</button>
     </div>
-    
   )
 }
 
